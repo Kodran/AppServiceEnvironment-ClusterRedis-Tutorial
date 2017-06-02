84b1c304fc2b7be4851a96f46d05bb299aa25a3c
Jorge Luis Castro Felix <jorge.castro.felix@outlook.com>
2017-06-02 01:58:21 -0500

    Redis controller added

---
+++ MyAPI.Demo/Controllers/RedisController.cs
@@ -0,0 +1,49 @@
+﻿using MyAPI.Demo.Models;
+using StackExchange.Redis;
+using Swashbuckle.Swagger.Annotations;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace MyAPI.Demo.Controllers
+{
+    public class RedisController : ApiController
+    {
+        private static string redisClusterConnection = "My StackExchange.Redis primary connection string";
+        public static IDatabase redisCache = ConnectionMultiplexer.Connect(redisClusterConnection).GetDatabase();
+
+        [SwaggerOperation("GetByKey")]
+        [SwaggerResponse(HttpStatusCode.OK)]
+        [SwaggerResponse(HttpStatusCode.NotFound)]
+        public async Task<IHttpActionResult> Get(string key)
+        {
+            var result = string.Empty;
+            try
+            {
+                result = await redisCache.StringGetAsync(key);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return Ok(result);
+        }
+
+        [SwaggerOperation("Create")]
+        [SwaggerResponse(HttpStatusCode.Created)]
+        public async Task<IHttpActionResult> Post([FromBody]CreateRequest request)
+        {
+            var result = false;
+            try
+            {
+                result =  await redisCache.StringSetAsync(request.key, request.value);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return Ok(result);
+        }
+    }
+}
---
+++ MyAPI.Demo/Models/CreateRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace MyAPI.Demo.Models
+{
+    public class CreateRequest
+    {
+        public string key { get; set; }
+        public string value { get; set; }
+    }
+}
